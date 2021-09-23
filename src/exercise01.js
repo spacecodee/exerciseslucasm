9874eb23193df60de50b862997bf2e42c56520f2
Space Codee <spacecodee@gmail.com>
2021-09-23 10:37:00 -0500

    add exercise 1

---
+++ src/exercise01.js
@@ -0,0 +1,26 @@
+// sumatoria de
+const textArea = document.getElementById("textArea");
+const btnCalc = document.getElementById("btnCalc");
+const nums = [];
+let suma = 0;
+
+const sumatoria = () => {
+  for (let i = 0; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      nums.push(i);
+    }
+  }
+};
+
+btnCalc.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  sumatoria();
+  nums.forEach((num) => {
+    suma += num;
+  });
+
+  textArea.innerHTML = `Los números de 3 y 5: \n${nums.toString()}, \nel total de números es: ${
+    nums.length
+  }, \ny la suma de todos es: ${suma}`;
+});
